0e2f4f5f3de81684b54e837441f80c1d02c74c71
David F <david.fitoussi@decathlon.com>
2022-05-18 21:20:05 +0200

    fix(discordClient): error message handler

--- src/discordLogClient.js
+++ src/discordLogClient.js
@@ -9,7 +9,7 @@ module.exports = {
   logError(message) {
     console.log(message)
     if(configuration.discordLogLevel.error) {
-      client.channels.fetch(idChannelAlerting).then(channel => channel.send(message));
+      client.channels.fetch(idChannelAlerting).then(channel => channel.send(`@everyone ${message.message}`));
     }
   },
   logInfo(message) {
