bc1380411c8309d83fdfda62e435b90d8714ea1c
David F <david.fitoussi@decathlon.com>
2022-06-06 10:51:21 +0200

    fix(resumeListing): direct relisting when calling commands

--- src/discord/commands/resumeLending.js
+++ src/discord/commands/resumeLending.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const configurationManager = require('../../../configurationManager');
+const configuration = require('../../../configuration');
+const gotchiManager = require('../../gotchiManager');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,6 +9,9 @@ module.exports = {
   .setDescription('Start or restart lending for all gotchis managed'),
   async execute(interaction) {
     configurationManager.toggleLendingListing(true)
+    for (const gotchi of configuration.gotchis) {
+      gotchiManager.lendGotchi(gotchi)
+    }
     return interaction.reply('Lending resumed and listing restarted for next routine iteration!');
     },
 };
